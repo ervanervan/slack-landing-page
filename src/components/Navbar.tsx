bd0f808848e48a74f21610008e44862e8f506316
ervanervan <ervanervan040@gmail.com>
2025-01-16 21:29:57 +0700

    update: navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,34 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.svg";
 
 const Navbar = () => {
   return (
-    <nav className="bg-purple-700 text-white py-4">
+    <header className="bg-custom-white text-black py-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-xl font-bold">
-          My App
-        </Link>
-        <ul className="flex space-x-4">
-          <li>
-            <Link to="/" className="hover:underline">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/about" className="hover:underline">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact" className="hover:underline">
-              Contact
-            </Link>
-          </li>
-        </ul>
+        <div>
+          <img src={logo} alt="" />
+        </div>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <Link to="/" className="hover:underline">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="hover:underline">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="hover:underline">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-    </nav>
+    </header>
   );
 };
 
