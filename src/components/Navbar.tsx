f8b9be54af463c3a26c7480a41d0635f36ecff55
ervanervan <ervanervan040@gmail.com>
2025-01-17 20:39:15 +0700

    update: navbar and add icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,53 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/images/logo.svg";
+import logo from "../assets/images/logo.png";
+import SearchIcon from "../assets/icons/SearchIcon";
+import SaleIcon from "../assets/icons/SaleIcon";
 
 const Navbar = () => {
   return (
-    <header className="bg-custom-white text-black py-4">
-      <div className="container mx-auto flex justify-between items-center">
+    <header className="bg-custom-white text-black border-b-2 border-black">
+      <div className="container mx-auto flex justify-between items-center h-[4.5rem]">
         <div>
-          <img src={logo} alt="" />
+          <img src={logo} alt="" className="w-32" />
         </div>
         <nav>
           <ul className="flex space-x-4">
             <li>
               <Link to="/" className="hover:underline">
-                Home
+                Products
               </Link>
             </li>
             <li>
               <Link to="/about" className="hover:underline">
-                About
+                Solutions
               </Link>
             </li>
             <li>
               <Link to="/contact" className="hover:underline">
-                Contact
+                Enterprise
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="hover:underline">
+                Resources
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="hover:underline">
+                Pricing
               </Link>
             </li>
           </ul>
         </nav>
+        <div className="flex items-center border-2 border-x-black h-full">
+          <div className="border-2 border-r-black h-full flex items-center px-4">
+            <SearchIcon />
+          </div>
+          <div className="flex items-center gap-2">
+            <SaleIcon />
+            <p>Talk To Sales</p>
+          </div>
+        </div>
       </div>
     </header>
   );
