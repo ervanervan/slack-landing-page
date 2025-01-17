e22f7c31030a32c38a1783f187fc3cb0d6c4825b
ervanervan <ervanervan040@gmail.com>
2025-01-17 21:02:45 +0700

    update: navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,45 +7,50 @@ const Navbar = () => {
   return (
     <header className="bg-custom-white text-black border-b-2 border-black">
       <div className="container mx-auto flex justify-between items-center h-[4.5rem]">
-        <div>
-          <img src={logo} alt="" className="w-32" />
+        <div className="flex items-center gap-4">
+          <div>
+            <img src={logo} alt="" className="w-32" />
+          </div>
+          <nav>
+            <ul className="flex space-x-4">
+              <li>
+                <Link to="/" className="hover:underline">
+                  Products
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" className="hover:underline">
+                  Solutions
+                </Link>
+              </li>
+              <li>
+                <Link to="/contact" className="hover:underline">
+                  Enterprise
+                </Link>
+              </li>
+              <li>
+                <Link to="/contact" className="hover:underline">
+                  Resources
+                </Link>
+              </li>
+              <li>
+                <Link to="/contact" className="hover:underline">
+                  Pricing
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <nav>
-          <ul className="flex space-x-4">
-            <li>
-              <Link to="/" className="hover:underline">
-                Products
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" className="hover:underline">
-                Solutions
-              </Link>
-            </li>
-            <li>
-              <Link to="/contact" className="hover:underline">
-                Enterprise
-              </Link>
-            </li>
-            <li>
-              <Link to="/contact" className="hover:underline">
-                Resources
-              </Link>
-            </li>
-            <li>
-              <Link to="/contact" className="hover:underline">
-                Pricing
-              </Link>
-            </li>
-          </ul>
-        </nav>
         <div className="flex items-center border-2 border-x-black h-full">
           <div className="border-2 border-r-black h-full flex items-center px-4">
             <SearchIcon />
           </div>
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 px-4">
             <SaleIcon />
             <p>Talk To Sales</p>
+          </div>
+          <div className="flex items-center gap-2 px-4 border-2 border-l-black h-full bg-black text-custom-white">
+            <p>Login / Register</p>
           </div>
         </div>
       </div>
