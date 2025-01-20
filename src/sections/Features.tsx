1b3316e507b3fa8b9e219e86ffb35567379dc15e
ervanervan <ervanervan040@gmail.com>
2025-01-20 16:38:26 +0700

    update: features content

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -11,20 +11,22 @@ const Features: React.FC = () => {
       <div className="container mx-auto px-4 space-y-14">
         <FeatureCard
           title="Bring Your Team Together"
-          description="At the heart of Slack are channels: organized spaces for everyone and everything you need for work. In channels, it&apos;s easier to connect across departments, offices, time zones and even other companies."
+          description="At the heart of Slack are channels: organized spaces for everyone and everything you need for work. In channels, it's easier to connect across departments, offices, time zones and even other companies."
           imgSrc={featureImg1}
           imgAlt="Channels"
           imagePosition="left"
           button="More about channels"
         />
+
         <FeatureCard
           title="Choose how you want to work"
-          description="In Slack, you&apos;ve got all the flexibility to work when, where and how it&apos;s best for you. You can easily chat, send audio and video clips, or hop on a huddle to talk things out live."
+          description="In Slack, you've got all the flexibility to work when, where and how it's best for you. You can easily chat, send audio and video clips, or hop on a huddle to talk things out live."
           imgSrc={featureImg2}
           imgAlt="Flexible"
           imagePosition="right"
           button="Flexible Communication"
         />
+
         <FeatureCard
           title="Move faster with your tools in one place"
           description="With your other work apps connected to Slack, you can work faster by switching tabs less. And with powerful tools like Workflow Builder, you can automate away routine tasks."
