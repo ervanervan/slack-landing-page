d3a1d40a728f951bf133572bb7b824627623b4a0
ervanervan <ervanervan040@gmail.com>
2025-01-21 21:02:17 +0700

    update: articles section

--- src/sections/Articles.tsx
+++ src/sections/Articles.tsx
@@ -1,7 +1,14 @@
 const Articles = () => {
   return (
-    <section className="py-16 bg-custom-gray-light bg-sigma-pattern bg-repeat bg-[length:450px]">
-      <div></div>
+    <section className="relative py-16 bg-custom-gray-light">
+      <div className="absolute inset-0 bg-sigma-pattern bg-repeat bg-[length:450px] opacity-20"></div>
+      <div className="relative z-10 container mx-auto px-4">
+        <h2 className="text-center font-bold text-2xl text-white">
+          Take a{" "}
+          <span className="text-custom-yellow font-extrabold">deeper dive</span>{" "}
+          into a new way to work
+        </h2>
+      </div>
     </section>
   );
 };
