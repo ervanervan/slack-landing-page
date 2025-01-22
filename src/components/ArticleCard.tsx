411d0a80d564b7091caee59968dc9eb4cc52b2b0
ervanervan <ervanervan040@gmail.com>
2025-01-22 12:59:48 +0700

    update: articles section and article card component

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -11,7 +11,6 @@ type ArticleCardProps = {
 
 const ArticleCard: React.FC<ArticleCardProps> = ({
   title,
-  description,
   image,
   category,
   link,
@@ -19,19 +18,23 @@ const ArticleCard: React.FC<ArticleCardProps> = ({
 }) => {
   return (
     <div
-      className={`relative rounded-3xl border-2 border-black p-4 transition-all duration-300 shadow-[2px_2px_0px_rgba(0,0,0,1)] cursor-pointer 
+      className={`relative rounded-3xl border-2 border-black px-3 py-4 transition-all duration-300 shadow-[2px_2px_0px_rgba(0,0,0,1)] cursor-pointer 
       ${isFeatured ? "bg-yellow-400 hover:bg-yellow-500" : "bg-white"} 
-      w-52 h-72`}
+      w-full md:w-52 h-72 flex flex-col justify-between`}
     >
-      <img
-        src={image}
-        alt={title}
-        className="w-full h-32 object-cover rounded-lg"
-      />
-      <span className="text-xs font-medium mt-5 block">{category}</span>
-      <h3 className="font-bold mt-1">{title}</h3>
-      <p className="text-gray-700 text-sm mt-1">{description}</p>
-      <a href={link} className="text-black font-bold mt-2 inline-block">
+      <div>
+        <img
+          src={image}
+          alt={title}
+          className="w-full h-28 object-cover rounded-xl"
+        />
+        <span className="text-xs font-medium mt-5 block">{category}</span>
+        <h3 className="font-bold text-sm mt-1">{title}</h3>
+      </div>
+      <a
+        href={link}
+        className="text-black text-sm font-bold inline-block self-end"
+      >
         {isFeatured ? "See All >" : "Read More >"}
       </a>
     </div>
--- src/sections/Articles.tsx
+++ src/sections/Articles.tsx
@@ -5,13 +5,13 @@ const Articles = () => {
   return (
     <section className="relative py-16 bg-custom-gray-light">
       <div className="absolute inset-0 bg-sigma-pattern bg-repeat bg-[length:450px] opacity-20"></div>
-      <div className="relative z-10 container mx-auto px-4">
+      <div className="relative z-10 max-w-[61.25rem] mx-auto px-4 lg:px-0">
         <h2 className="text-center font-bold text-2xl text-white custom-text-shadow mb-16">
           Take a{" "}
           <span className="text-custom-yellow font-extrabold">deeper dive</span>{" "}
           into a new way to work
         </h2>
-        <div className="flex items-center justify-center gap-12">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 place-items-center gap-8 md:gap-12">
           {articleList.map((article, index) => (
             <ArticleCard key={index} {...article} />
           ))}
