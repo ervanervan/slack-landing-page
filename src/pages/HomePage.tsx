759d4d46a23f116feb2f76ad2e907e4558fa4a26
ervanervan <ervanervan040@gmail.com>
2025-01-22 13:05:54 +0700

    add: promo and testimonial section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,8 @@ import Articles from "../sections/Articles";
 import Companies from "../sections/Companies";
 import Features from "../sections/Features";
 import Hero from "../sections/Hero";
+import Promo from "../sections/Promo";
+import Testimonial from "../sections/Testimonial";
 
 const HomePage = () => {
   return (
@@ -9,6 +11,8 @@ const HomePage = () => {
       <Hero />
       <Companies />
       <Features />
+      <Promo />
+      <Testimonial />
       <Articles />
     </>
   );
