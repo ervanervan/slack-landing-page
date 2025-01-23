c34fd8608864c2bd73928f22ba74d89e3b92511a
ervanervan <ervanervan040@gmail.com>
2025-01-23 20:46:46 +0700

    update: promo and testimonial section

--- src/sections/Promo.tsx
+++ src/sections/Promo.tsx
@@ -1,5 +1,31 @@
+import slackText from "../assets/images/slack-text.svg";
+import Button from "../components/Button";
+
 const Promo = () => {
-  return <div>Promo</div>;
+  return (
+    <section className="py-16">
+      <div className="flex flex-col items-center justify-center">
+        <div>
+          <div className="flex items-end justify-center">
+            <h2 className="font-semibold text-[1.75rem]">
+              Teams <span className="font-extrabold">large</span> and small rely
+              on
+            </h2>
+            <img src={slackText} alt="" className="w-36 mb-2.5" />
+          </div>
+          <p className="font-semibold text-lg text-center mt-3 w-full md:max-w-[37rem] mx-auto">
+            Slack securely scales up to support collaboration at the
+            world&apos;s biggest companies.
+          </p>
+        </div>
+        <div className="mt-14 flex items-center gap-3">
+          <Button variant="secondary">Meet Slack For Enterprise</Button>
+          <Button variant="primary">Talk to Sales</Button>
+        </div>
+        <div className="mt-14 flex items-center justify-center gap-24"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Promo;
--- src/sections/Testimonial.tsx
+++ src/sections/Testimonial.tsx
@@ -1,5 +1,33 @@
+import promoVideo from "../assets/images/promo-video.png";
+import vectorTriangle from "../assets/images/vector-triangle.svg";
+
 const Testimonial = () => {
-  return <div>Testimonial</div>;
+  return (
+    <section className="max-w-[980px] mx-auto p-4 md:p-0">
+      <div className="relative flex flex-col md:flex-row items-center justify-center gap-y-4">
+        <img
+          src={vectorTriangle}
+          alt=""
+          className="absolute top-0 -left-16 -z-10"
+        />
+        <img src={promoVideo} alt="" className="w-[26rem]" />
+        <div className="flex flex-col md:p-3">
+          <p className="text-2xl font-medium">
+            “We were able to create a large virtual network of employees that
+            can communicate as though they are together. There was a lot of
+            disruption in terms of where we worked, but in terms of how we
+            worked—very little disruption.”
+          </p>
+          <div>
+            <h3 className="font-bold text-xl mt-3">Mark Smith</h3>
+            <p className="text-lg">
+              Senior Technical Product Manager, T-Mobile
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonial;
