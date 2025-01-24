00221c33821b735e8754d6d7f14b55293eb0b325
ervanervan <ervanervan040@gmail.com>
2025-01-24 10:29:54 +0700

    update: stat card component

--- src/components/StatCard.tsx
+++ src/components/StatCard.tsx
@@ -8,14 +8,16 @@ type StatProps = {
 
 const StatCard: React.FC<StatProps> = ({ percentage, text, color }) => {
   return (
-    <div className="flex flex-col items-center text-center">
+    <div className="flex flex-col items-center md:items-start text-center md:text-start">
       <span
         className={`text-5xl font-extrabold font-karla mb-3 custom-text-shadow`}
         style={{ color }}
       >
         {percentage}
       </span>
-      <p className="text-sm font-medium max-w-72 font-karla">{text}</p>
+      <p className="text-sm font-medium max-w-72 md:max-w-xs font-karla">
+        {text}
+      </p>
     </div>
   );
 };
