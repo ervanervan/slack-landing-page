dd3109ef7daf9460f386d0728e49042ccb50f2a2
ervanervan <ervanervan040@gmail.com>
2025-01-25 13:59:30 +0700

    update: homepage pages

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,8 @@
+import Hero from "../sections/Hero";
+import Promo from "../sections/Promo";
 import Articles from "../sections/Articles";
 import Companies from "../sections/Companies";
 import Features from "../sections/Features";
-import Hero from "../sections/Hero";
-import Promo from "../sections/Promo";
 import Testimonial from "../sections/Testimonial";
 
 const HomePage = () => {
